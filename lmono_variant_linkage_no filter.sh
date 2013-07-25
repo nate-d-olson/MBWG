4c8c845d416efeafb29505c3b9e38a46db443a9a
nate-d-olson <nolson@nist.gov>
2013-07-25 15:26:10 -0400

    Delete lmono_variant_linkage_no filter.sh

--- lmono_variant_linkage_no filter.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-for bam in *F.bam;
-do
-	sam=$(echo $bam | sed 's/bam/sam/')
-	csv=$(echo $bam | sed 's/bam/csv/')
-	samtools view -o $sam $bam
-	python SAM_sequencingError_parser.py $sam Lmono_16S_consensus.fasta $csv
-done
-
