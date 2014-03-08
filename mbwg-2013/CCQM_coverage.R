c3ddd28717a6d0be412028a116b28208d9d4ce26
Nate Olson <nolson@nist.gov>
2014-03-08 15:40:16 -0500

    commit changes for new directory with new project structure

--- mbwg-2013/CCQM_coverage.R
+++ mbwg-2013/CCQM_coverage.R
@@ -1,52 +1,52 @@
-##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#########################%%%%%%%
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#########################%%%%%%%
 ##
 ## Coverage plot for CCQM identity work
-## written by: Nate Olson
-## May 2013
-## Modified 7/20/2013 - cleanup for release on github
-##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#########################%%%%%%%
+##
+##
+##
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#########################%%%%%%%
+
 
 library(ggplot2)
 library(reshape2)
+library(plyr)
 
-# set working directory for CCQM folder with pileup.csv files (generated using ccqm_pipe.sh)
-# will need to change to the appropriate location for local computer
-setwd("/media/second/mirror/CCQM/Identity-Study-I/CCQM_Analysis_Results/pileup_parse/")
+#set working directory for CCQM folder with pileup.csv, .tsv, and vcf files
+setwd("/Volumes/UNTITLED/CCQM/ccqm_proportions_analysis/")
 
 #######
 ## Import of pileup files parsed using PileUpParse.pl
 ## input file names need to be the following format or change the input loop below
-## ORG-PLATFORM-LAB-REPLICATE.pileup.csv
+## ORG_LAB_PLATFORM_.pileup
 #######
 
-pileup_csv <- list.files()[grep("F.pileup.csv", c(list.files()), value = F)]
+pileup_csv <- list.files()[grep("-1.pileup.csv", c(list.files()), value = F)]
 
 #generating a dataframe combining the pileup parse files
 pileup = data.frame()
 for(i in 1:length(pileup_csv)){
-  fileTable <- read.csv(pileup_csv[i], header = T)
-  dataset <- gsub(".pileup.csv", "", i)
+  fileTable <- read.csv(pileup_csv[i], header = F, stringsAsFactors=F)
+  colnames(fileTable) <- c("Control","Location","ref","string","cigar","blank","Coverage","A","C","G","T")
   id <- strsplit(pileup_csv[i], "-")
   fileTable$org <- id[[1]][1]
   fileTable$lab <- id[[1]][3]
   fileTable$platform <- id[[1]][2]
+  fileTable$rep <- id[[1]][4]
   fileTable$data_set <- pileup_csv[i]
   pileup <- rbind(pileup, fileTable)
 }
 remove(fileTable, i, id, pileup_csv)
 pileup <- subset(pileup, select = -c(Control))
 
+pileup$Location <- as.numeric(pileup$Location)
 ggplot(pileup) + 
-  geom_path(aes(x = Location, 
-		y = Coverage, 
-		color = lab, 
-		linetype = org, 
-		group = data_set)) + 
+  geom_path(aes(x = Location, y = Coverage, color = lab, 
+                linetype = org, group = data_set)) + 
   facet_wrap(~platform) + scale_y_log10() + 
-  labs(y = "Coverage", x = "Reference Location (bp)", color = "Participants", linetype = "Organisms") +
-  theme_bw() +
-  theme(legend.position = "bottom", legend.direction = "horizontal")
+  labs(y = "Coverage (log10)", x = "Reference Location (bp)")
 
-
-
-
+ggplot(pileup[pileup$platform == "454",]) + 
+  geom_path(aes(x =Location, y= Coverage, color = lab, 
+                linetype = org, group = data_set)) +
+  scale_y_log10() + 
+  labs(y = "Coverage (log10)", x = "Reference Location (bp)")
--- mbwg-2013/subsample_proportions_analysis.R
+++ mbwg-2013/subsample_proportions_analysis.R
@@ -1,34 +1,15 @@
-##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#########################%%%%%%%
-##
-## subsampled proportion analysis and plots for subsampled biologically variant positions
-## written by: Nate Olson
-## May 2013
-## Modified 7/20/2013 - cleanup for release on github
-##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#########################%%%%%%%
-
-# required packages
+setwd("~/Documents/CCQM/ccqm_proportions_analysis/")
 library(ggplot2)
 library(reshape2)
 library(plyr)
 
-# set working directory for variant_analysis folder
-# will need to change to the appropriate location for local computer
-#setwd("~/Documents/mirror/CCQM/Identity-Study-I/ccqm_biovar_analysis/")
+#set working directory for variant_analysis folder
 
-
-#%^)(*&^%$%^&*()(*&^% 
-#$%^&*()(*&^&*()*&^%^&*()(*&^   temporary - will change before saving 1/6/2014
-
-
-
-setwd("~/Desktop/to_file/ccqm_biovar_analysis/")
-pileup_csv <- list.files()[grep(".pileup.csv$", c(list.files()), value = F)]
-
+pileup_csv <- list.files()[grep("csv$", c(list.files()), value = F)]
 #%#%#% generating a dataframe combining the pileup parse files
 
 pileup_parse = data.frame()
 for(i in pileup_csv){
-  print(i)
   fileTable <- read.table(i, header = F, sep = ",")
     colnames(fileTable) <- c("Reference","Location", "Ref","String","Qual", "blank","depth","A","C","G","T") 
     dataset <- gsub(".pileup.csv", "", i)
@@ -38,17 +19,29 @@ for(i in pileup_csv){
     fileTable$lab <- id[[1]][3]
     fileTable$platform <- id[[1]][2]
     fileTable$rep <- id[[1]][4]
+    fileTable$strain <- id[[1]][5]
+    fileTable$trim <- id[[1]][6]
     fileTable$frac <- id[[1]][7]
     fileTable <- subset(fileTable[!(fileTable$Ref %in% c("A","T","C","G")),], select = -c(String,Qual,blank))
     pileup_parse <- rbind(pileup_parse, fileTable)
+  }
 }
-
-
 pileup_parse <- pileup_parse[pileup_parse$Ref != "Traversal" & pileup_parse$Location %in% 100:1400,]
+write.csv(pileup_parse, "pileup_counts_variant_positions_only.csv")
+
+# reading proportion count file
+pileup_parse <- read.csv("pileup_counts_variant_positions_only.csv")
+
 pileup_parse$Location <- as.numeric(as.character(pileup_parse$Location))
 
 
-# list of biologically variant positions and potential alleles
+# statistical analysis -bayes
+#ambiguities
+R <- c("A","G")
+S <- c("C","G")
+Y <- c("C","T")
+W <- c("A","T")
+K <- c("G","T")
 ambigs <- list(
   "118"=c("A","G"),
   "975"=c("G","A"),
@@ -65,7 +58,6 @@ ambigs <- list(
   "188"=c("C","T"),
   "419"=c("A","G"))
 
-# bayesian analysis
 max_copy = NULL
 max_prob = NULL
 prop = NULL
@@ -99,13 +91,38 @@ for(i in 1:nrow(pileup_parse)){
 }
 pileup_parse <- data.frame(cbind(pileup_parse, prop, max_copy, max_prob))
 
+
+library(ggplot2)
+#ggplot(pileup_parse) + geom_jitter(aes(x = Location, y = prop, color = log(depth))) + facet_wrap(~org, scale = "free") 
+#ggplot(pileup_parse) + geom_point(aes(depth, prop)) + xlim(0,200) + facet_wrap(~Location)
+#ggplot(pileup_parse[pileup_parse$Location %in% 0:1400,]) + geom_point(aes(depth, prop)) + xlim(0,100) + facet_wrap(~Location)
+#ggplot(pileup_parse) + geom_smooth(aes(depth, prop)) + scale_x_log10() + facet_wrap(~Location)
+
 pileup_parse$max_copy <- as.factor(pileup_parse$max_copy)
+ggplot(pileup_parse) + geom_point(aes(depth, prop, color = max_copy, shape = platform)) + 
+  scale_x_log10() + facet_wrap(~org*Location)
+ggplot(pileup_parse[pileup_parse$depth < 200,]) + geom_point(aes(depth, prop, color = max_copy, shape = platform)) + 
+  #scale_x_log10() + 
+  facet_wrap(~org*Location)
+
+ggplot(pileup_parse[pileup_parse$depth < 200 & pileup_parse$org == "Ecoli",]) + geom_point(aes(depth, prop, color = max_copy, shape = platform)) + 
+  facet_wrap(~org*Location)
+
+ggplot(pileup_parse[pileup_parse$depth < 200 & pileup_parse$org == "Lmono",]) + geom_point(aes(depth, prop, color = max_copy, shape = platform)) + 
+  facet_wrap(~org*Location)
 
 pileup_parse$exp[pileup_parse$Location %in% c(975,979,983,992,993,994,995,996,1011)] <- "6:1" 
 pileup_parse$exp[pileup_parse$Location %in% c(118,1395)] <- "4:3"
 pileup_parse$exp[pileup_parse$Location %in% c(118,1395)] <- "4:3"
 pileup_parse$exp[pileup_parse$Location %in% c(175,419)] <- "5:1"
 pileup_parse$exp[pileup_parse$Location == 188] <- "3:3"
+
+ggplot(pileup_parse) + geom_point(aes(depth, prop, color = max_copy, shape = platform)) + 
+  scale_x_log10() +
+  facet_wrap(~org*exp)
+
+ggplot(pileup_parse[pileup_parse$depth < 200,]) + geom_point(aes(depth, prop, color = max_copy, shape = platform)) + 
+  facet_wrap(~org*exp)
 
 high <- c("6:1","5:1")
 even <- c("4:3","3:3")
@@ -118,21 +135,19 @@ ggplot(pileup_parse[pileup_parse$exp %in% even,]) + geom_point(aes(depth, prop, 
   scale_x_log10() + geom_vline(aes(xintercept = 200), linetype = 2, alpha = 0.5) + facet_wrap(~exp) +
   labs (x = "log(Depth)", y = "Observed Proportions", shape = "Sequencing\nPlatform", color = "Copy Ratio")
 
-library("Hmisc")
-pileup_parse$depth_groups <- as.numeric(cut2(pileup_parse$depth))
-library("plyr")
-ggplot(pileup_parse) + geom_histogram(aes(x = depth_groups)) + facet_wrap(~exp)
-depth_decay <- ddply(pileup_parse,.(exp, depth_groups), summarise, decay = sd(prop), depth = mean(depth), size = length(platform))
-ggplot(depth_decay) + geom_point(aes(x = depth, y= decay, color = exp)) + 
-  geom_smooth(aes(x = depth, y= decay, color = exp)) + scale_x_log10() + 
-  theme_bw() + 
-  theme(legend.position = "bottom", legend.direction = "horizontal") +
-  labs(y = "Standard Deviation of Observed Proportion", x = "Coverage", color = "Expected Ratio")
+ggplot(pileup_parse) + geom_point(aes(depth, prop, color = max_copy, shape = platform)) + 
+  scale_x_log10() + facet_wrap(~exp, nrow = 1) +
+  labs (x = "log(Depth)", y = "Observed Proportions", shape = "Sequencing\nPlatform", color = "Copy Ratio")
 
-ddecay <- ddply(pileup_parse, .(exp), depth_groups = as.numeric(cut(depth, 30))) 
-decay_sd <- ddply(pileup_parse, .(exp, depth_groups), summarize, depth = median(depth), decay = sd(prop))
-ggplot(decay_sd) + geom_point(aes(x = depth, y= decay)) + 
-  geom_smooth(aes(x = depth, y= decay)) + scale_x_log10() + 
-  theme_bw() + 
-  labs(y = "Standard Deviation of Observed Proportion") +
-  facet_wrap(~exp)
+pileup_parse$max_copy <- as.factor(pileup_parse$max_copy)
+pileup_parse <- cbind(pileup_parse, colsplit(pileup_parse$exp, ":", names = c("high", "low")))
+ggplot(pileup_parse) + 
+  geom_hline(aes(yintercept = 100), linetype = 2) +
+  geom_hline(aes(yintercept = 500), linetype = 2) +
+  geom_boxplot(data= pileup_parse[pileup_parse$max_copy == pileup_parse$high,], 
+                                    aes(max_copy, depth, color = max_copy)) +
+  geom_boxplot(data= pileup_parse[pileup_parse$max_copy != pileup_parse$high,], 
+              aes(max_copy, depth)) +
+  geom_hline(aes(yintercept = 100, label = 100), linetype = 2) +
+  facet_wrap(~exp, nrow = 1) + scale_y_log10() + 
+  labs (y = "log(Depth)", x = "Copy Ratio", shape = "Sequencing\nPlatform", color = "Copy Ratio")
